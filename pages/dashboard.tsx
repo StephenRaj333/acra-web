b7290f590c2c7033ab1af6679fb0c4698ddefb5d
stephen <stephenvijay093@gmail.com>
2026-03-18 09:08:59 +0530

    time setting

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -47,7 +47,8 @@ const formatDateTime = (value: any): string => {
       hour: '2-digit',
       minute: '2-digit',
       second: '2-digit',
-      hour12: true
+      hour12: true,
+      timeZone: 'Asia/Kolkata'
     }).format(date)
   } catch {
     return String(value)
